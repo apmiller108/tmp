144d3617c971951db51922b102ba38205b4aaa81
Alex Miller <apmiller@covermymeds.com>
2023-04-21 15:51:09 -0500

    Adds token generation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
          :trackable, :validatable
+
+  def generate_jwt
+    JWT.encode(
+      { email: email, exp: 30.days.from_now.to_i },
+      Rails.application.secrets.secret_key_base,
+      'HS256'
+    )
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#generate_jwt' do
+    let(:user) { build(:user, email: 'foo@example.com') }
+
+    subject(:jwt) { user.generate_jwt }
+
+    it 'returns a JWT with a payload containing the email' do
+      payload = JWT.decode(jwt, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')[0]
+      expect(payload['email']).to eq user.email
+    end
+  end
 end
