c705ff9e3183f3da8c6d3e2b3775ce6817663078
Alex Miller <apmiller@covermymeds.com>
2023-04-21 15:49:54 -0500

    Adds jwt and authenticate json format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def authenticate_user!
+    if request.format.json?
+      # TODO: authenticate_with_jwt
+    else
+      super
+    end
+  end
 end
