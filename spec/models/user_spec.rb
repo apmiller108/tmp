d5d3263a608ccaa8654b2c1641c4cfd9af5bf4d4
Alex Miller <apmiller@covermymeds.com>
2023-05-19 15:28:06 -0500

    Deletes obsolete spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe '#generate_jwt' do
-    let(:user) { build(:user, email: 'foo@example.com') }
-
-    subject(:jwt) { user.generate_jwt }
-
-    it 'returns a JWT with a payload containing the email' do
-      payload = JWT.decode(jwt, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')[0]
-      expect(payload['email']).to eq user.email
-    end
-  end
 end
