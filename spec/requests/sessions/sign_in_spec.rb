76fdd876bcaaf38f3296d4987c4d9f1d46c2e9be
Alex Miller <apmiller@covermymeds.com>
2023-05-26 14:50:21 -0500

    Updates sign in spec

--- spec/requests/sessions/sign_in_spec.rb
+++ spec/requests/sessions/sign_in_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Sessions', type: :request do
+RSpec.describe 'users/sign_in', type: :request do
   let!(:user) { create :user }
   let(:password) { user.password }
 
@@ -9,7 +9,7 @@ RSpec.describe 'Sessions', type: :request do
       post '/users/sign_in.json', params: { user: { email: user.email, password: password } }
     end
 
-    it 'returns a successful response' do
+    it 'returns a success response' do
       expect(response).to have_http_status(:success)
     end
 
@@ -17,11 +17,35 @@ RSpec.describe 'Sessions', type: :request do
       expect(response.headers['Authorization']).not_to be_nil
     end
 
-    context 'with in valid credientials' do
+    it 'returns the user JSON' do
+      expect(JSON.parse(response.body)).to eq user.as_json(only: %i[email id])
+    end
+
+    context 'with invalid credientials' do
       let(:password) { Faker::Internet.password }
 
       it 'returns an unauthorized response' do
         expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+
+  describe 'POST /users/sign_in' do
+    before do
+      post '/users/sign_in', params: { user: { email: user.email, password: password } }
+    end
+
+    it { is_expected.to redirect_to "/" }
+
+    it 'shows a success message' do
+      expect(flash[:notice]).to eq 'Signed in successfully.'
+    end
+
+    context 'with invalid credientials' do
+      let(:password) { Faker::Internet.password }
+
+      it 'shows an error message' do
+        expect(flash[:alert]).to eq 'Invalid Email or password.'
       end
     end
   end
