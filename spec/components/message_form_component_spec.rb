bbc834bd2df8ff151d587687d8e669dcfc6bb745
Alex Miller <apmiller108@yahoo.com>
2023-11-22 21:21:50 -0600

    Adds spec for MessageFormComponent

--- spec/components/message_form_component_spec.rb
+++ spec/components/message_form_component_spec.rb
@@ -1,15 +1,38 @@
-# frozen_string_literal: true
-
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe MessageFormComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { page }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  let(:message) { build_stubbed(:message, :with_user) }
+  let(:user) { message.user }
+  let(:component) { described_class.new(message:) }
+
+  class MockInlineFieldComponent < ApplicationViewComponent
+    TEXT = 'MockInlineFieldComponent'
+    renders_one :field_slot
+    haml_template <<~HAML
+      #{TEXT}
+      = field_slot
+    HAML
+  end
+
+  before do
+    allow(InlineFieldComponent).to receive(:new).and_return(MockInlineFieldComponent.new)
+    with_current_user(user) { render_inline(component) }
+  end
+
+  it { is_expected.to have_css "form[action='/users/#{user.id}/messages/#{message.id}']" }
+
+  it 'initializes the InlineFieldComponent with the proper arguments' do
+    expect(InlineFieldComponent).to(
+      have_received(:new)
+        .with(model: [user, message], attribute: :content, form: kind_of(ActionView::Helpers::FormBuilder))
+    )
+  end
+
+  it 'render the InlineFieldComponent' do
+    expect(page).to have_text(MockInlineFieldComponent::TEXT)
+  end
+
+  it { is_expected.to have_css 'trix-editor[id="message_content"]' }
 end
