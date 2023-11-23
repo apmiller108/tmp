b8ce5b85fe8bb27996155ac9cb715b2b2c5fd5f7
Alex Miller <apmiller108@yahoo.com>
2023-11-23 10:23:55 -0600

    Adds spec for InlineEditComponent

--- spec/components/inline_edit_component_spec.rb
+++ spec/components/inline_edit_component_spec.rb
@@ -3,13 +3,63 @@
 require "rails_helper"
 
 RSpec.describe InlineEditComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { page }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  let(:model) { build_stubbed(:user) }
+  let(:attribute) { :foo }
+  let(:field) { 'FIELD' }
+  let(:component) do
+    described_class.new(model:, attribute:).tap do |c|
+      c.with_field_slot { field }
+    end
+  end
+
+  describe '.turbo_frame_id' do
+    context 'with a single model' do
+      let(:dom_id) { ActionView::RecordIdentifier.dom_id(model) }
+
+      it 'creates a unique frame id' do
+        expect(described_class.turbo_frame_id(model, attribute)).to eq "turbo_frame_#{dom_id}_#{attribute}"
+      end
+    end
+
+    context 'with an array of models' do
+      let(:models) { [model, build_stubbed(:user)] }
+      let(:dom_ids) { models.map { |m| ActionView::RecordIdentifier.dom_id(m) }.join('_') }
+
+      it 'creates a unique frame id' do
+        expect(described_class.turbo_frame_id(models, attribute)).to eq "turbo_frame_#{dom_ids}_#{attribute}"
+      end
+    end
+  end
+
+  describe 'render' do
+    let(:model) { [build_stubbed(:user), build_stubbed(:message)] }
+    let(:turbo_frame_id) { 'turbo_frame_id' }
+
+    before do
+      allow(described_class).to receive(:turbo_frame_id).with(model, attribute).and_return(turbo_frame_id)
+      render_inline component
+    end
+
+    it 'renders a form with a turbo_frame data tag' do
+      expect(page).to have_css("form[action='#{polymorphic_path(model)}'][data-turbo-frame=#{turbo_frame_id}]")
+    end
+
+    it 'renders an inline edit turbo frame' do
+      expect(page).to have_css("turbo-frame.inline-edit[id='#{turbo_frame_id}']")
+    end
+
+    it 'renders the field' do
+      within('turbo-frame') do
+        expect(page).to have_content field
+      end
+    end
+
+    it 'renders an edit link' do
+      within('turbo-frame') do
+        expect(page).to have_link "#{I18n.t('edit')} #{attribute}", href: edit_polymorphic_path(model)
+      end
+    end
+  end
 end
