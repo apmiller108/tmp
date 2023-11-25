174b391578fe0242439e4e0cee042ba580015dc3
Alex Miller <apmiller108@yahoo.com>
2023-11-25 12:40:49 -0600

    Makes the cancel path dynamic

--- app/views/components/message_form_component.rb
+++ app/views/components/message_form_component.rb
@@ -10,4 +10,8 @@ class MessageFormComponent < ApplicationViewComponent
   def id
     dom_id(message)
   end
+
+  def cancel_path
+    message.persisted? ? user_message_path(current_user, message) : user_messages_path(current_user)
+  end
 end
