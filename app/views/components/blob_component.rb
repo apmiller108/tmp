125408730bf7445536f46ae850c19f6641a3e0df
Alex Miller <apmiller108@yahoo.com>
2023-12-16 23:48:12 -0600

    Cleans up component

--- app/views/components/blob_component.rb
+++ app/views/components/blob_component.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class BlobComponent < ApplicationViewComponent
+  DEFAULT_IMAGE_SIZE = [1024, 768].freeze
+  GALLERY_IMAGE_SIZE = [800, 600].freeze
+
   attr_reader :blob, :in_gallery
 
   delegate :audio?, :byte_size, :content_type, :filename, :representable?, :representation, :url, to: :blob
@@ -20,6 +23,10 @@ class BlobComponent < ApplicationViewComponent
 
   def humanized_file_size
     number_to_human_size byte_size
+  end
+
+  def resize_to_limit
+    in_gallery ? GALLERY_IMAGE_SIZE : DEFAULT_IMAGE_SIZE
   end
 
   def fig_caption
