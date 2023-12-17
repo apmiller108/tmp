193f9d66135cdc7a5e9d3b061d33f6006b40c16d
Alex Miller <apmiller108@yahoo.com>
2023-12-17 12:27:09 -0600

    Mounts sidekiq web ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
@@ -13,6 +15,11 @@ Rails.application.routes.draw do
     unauthenticated do
       root 'users/registrations#new'
     end
+  end
+
+  # Require authentication via devise and current_user be developer to view Sidekiq Web UI
+  authenticate :user, ->(user) { user.developer? } do
+    mount Sidekiq::Web => '/sidekiq'
   end
 
   resources :users, only: %i[show] do
