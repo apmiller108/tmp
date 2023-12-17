6552e2252c1611694231f485e876d5057115a08e
Alex Miller <apmiller108@yahoo.com>
2023-12-17 13:14:27 -0600

    Adds transcription service

---
+++ app/lib/transcription_service.rb
@@ -0,0 +1,16 @@
+class TranscriptionService
+  def self.batch_transcribe(client, blob)
+    new(client, blob).batch_transcribe
+  end
+
+  attr_reader :client, :blob
+
+  def initialize(client, blob)
+    @client = client
+    @blob = blob
+  end
+
+  def batch_transcribe
+    client.batch_transcribe(blob)
+  end
+end
