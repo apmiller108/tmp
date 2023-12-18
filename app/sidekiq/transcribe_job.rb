e674e7c03ae4789a5d31a0e0d204ac610a942938
Alex Miller <apmiller108@yahoo.com>
2023-12-17 21:43:14 -0600

    Adds todos

--- app/sidekiq/transcribe_job.rb
+++ app/sidekiq/transcribe_job.rb
@@ -4,9 +4,11 @@ class TranscribeJob
   def perform(blob_id, toxicity_detection: false)
     blob = ActiveStorage::Blob.find(blob_id)
 
+    # TODO: raise rescue and log this. Do not retry
     return unless blob.content_type =~ /\Aaudio/
 
     client = TranscriptionService::AwsClient.new(toxicity_detection:)
+    # TODO: handle job already created error. Rescue and do not retry
     response = TranscriptionService.batch_transcribe(client, blob)
     Rails.logger.info response.to_h
   end
