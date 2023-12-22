2dcce194b996ab40e5469ed44ec3ea9250f57126
Alex Miller <apmiller108@yahoo.com>
2023-12-21 21:54:52 -0600

    Removes columns default

--- db/migrate/20231220024655_create_transcription_jobs.rb
+++ db/migrate/20231220024655_create_transcription_jobs.rb
@@ -1,7 +1,7 @@
 class CreateTranscriptionJobs < ActiveRecord::Migration[7.1]
   def change
     create_table :transcription_jobs do |t|
-      t.text :status, null: false, default: 'created'
+      t.text :status, null: false
       t.check_constraint "status in ('created', 'queued', 'in_progress', 'failed', 'completed')", name: 'status_check'
       t.jsonb :response
       t.jsonb :request
