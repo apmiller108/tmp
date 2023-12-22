a6e8d044cf1380d4333198d0f6c2befe57d4a5a7
Alex Miller <apmiller108@yahoo.com>
2023-12-21 22:06:54 -0600

    Adds saftey nav

--- app/models/references_audio_blob_validator.rb
+++ app/models/references_audio_blob_validator.rb
@@ -1,6 +1,6 @@
 class ReferencesAudioBlobValidator < ActiveModel::Validator
   def validate(record)
-    return if record.respond_to?(:active_storage_blob) && record.active_storage_blob.audio?
+    return if record.respond_to?(:active_storage_blob) && record.active_storage_blob&.audio?
     record.errors.add :attachment, "must have audio content"
   end
 end
