8ffb9f90088acfa5db229b5bbece025f6f8be9f5
Alex Miller <apmiller108@yahoo.com>
2023-12-24 22:19:43 -0600

    Adds active storage extension

---
+++ app/models/concerns/active_storage_attachment_extension.rb
@@ -0,0 +1,11 @@
+module ActiveStorageAttachmentExtension
+  extend ActiveSupport::Concern
+
+  included do
+    # This supports doing active record joins to rich_text_content polymorphic
+    # association (via record_id/record_type)
+    # ex: `ActiveStorage::Attachments.joins(:rich_text_content))`
+    has_one :self_ref, class_name: 'ActiveStorage::Attachment', foreign_key: :id
+    has_one :rich_text_content, through: :self_ref, source: :record, source_type: 'ActionText::RichText'
+  end
+end
