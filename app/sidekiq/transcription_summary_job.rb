d30dac1e47c39498152254353cc69332a3c48c9f
Alex Miller <apmiller108@yahoo.com>
2024-01-01 19:30:58 -0600

    Adds transcription summary job

--- app/sidekiq/transcription_summary_job.rb
+++ app/sidekiq/transcription_summary_job.rb
@@ -2,23 +2,32 @@ class TranscriptionSummaryJob
   include Sidekiq::Job
 
   SUMMARY_PROMPT = <<~PROMPT.freeze
-    Summarize the following transcription into a series of bullet points:
+    Summarize the transcription below into a series of statements deliminated by "*" character.
 
     "%s"
   PROMPT
 
-
   def perform(user_id, transcription_id)
     user = User.find(user_id)
     transcription = user.transcriptions.find(transcription_id)
-    # TODO: Create summary model. Polymorphic belongs to summarizable.
-    # summary = transcription.build_summary
+    summary = transcription.summary
 
-    LLMService.invoke_model_stream(prompt: SUMMARY_PROMPT % transcription.content) do |stream_response|
-      # summary.content += stream_response.content
-      # TODO create summary component
-      # TODO broadcast summary component turbo stream
-      # summary.save! if stream_response.final_chunk?
+    summary.in_progress!
+
+    # @stream_response [InvokeModelStreamResponse, #content, #final_chunk?]
+    LLMService.new.invoke_model_stream(prompt: SUMMARY_PROMPT % transcription.content) do |stream_response|
+      summary.content += stream_response.content
+
+      if stream_response.final_chunk?
+        summary.status = Summary.statuses[:completed]
+        summary.save!
+      end
+
+      ViewComponentBroadcaster.call(
+        [user, TurboStreams::STREAMS[:blobs]],
+        component: TranscriptionSummaryComponent.new(transcription:),
+        action: :replace
+      )
     end
   end
 end
