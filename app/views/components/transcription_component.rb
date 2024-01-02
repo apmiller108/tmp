c18341c6d9fd131c7a2fa117c9b184a9793937d5
Alex Miller <apmiller108@yahoo.com>
2024-01-01 19:30:38 -0600

    Adds transcription summary component

--- app/views/components/transcription_component.rb
+++ app/views/components/transcription_component.rb
@@ -22,6 +22,10 @@ class TranscriptionComponent < ApplicationViewComponent
     "transcription_speakers_#{transcription.id}"
   end
 
+  def summary_dom_id
+    "transcription_summary_#{transcription.id}"
+  end
+
   def download_path
     user_transcription_download_path(current_user, transcription_id: transcription.id)
   end
---
+++ app/views/components/transcription_summary_component.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class TranscriptionSummaryComponent < ApplicationViewComponent
+  attr_reader :transcription
+
+  delegate :summary, to: :transcription
+  delegate :bullet_points?, :bullet_points, :content, :completed?, :created?, :queued?, :in_progress?,
+           to: :summary, allow_nil: true
+
+  def initialize(transcription:)
+    @transcription = transcription
+  end
+
+  def id
+    if summary
+      dom_id(summary)
+    else
+      "no_summary_#{transcription.id}"
+    end
+  end
+
+  def content
+  end
+
+  def generating_summary?
+    created? || queued? || in_progress?
+  end
+end
