30dfd487b7239a1dfb4bf7c69d1baa44918b3ece
Alex Miller <apmiller108@yahoo.com>
2024-01-07 14:24:38 -0600

    Includes url helpers for background job broadcasting

--- app/views/components/application_view_component.rb
+++ app/views/components/application_view_component.rb
@@ -1,5 +1,6 @@
 class ApplicationViewComponent < ViewComponent::Base
   include Dry::Effects.Reader(:current_user, default: nil)
+  include Rails.application.routes.url_helpers
 
   # See https://viewcomponent.org/compatibility.html#actiontext
   delegate :rich_text_area_tag, :turbo_frame_tag, to: :helpers
