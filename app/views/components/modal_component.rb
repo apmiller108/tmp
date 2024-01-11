97c25809ec8e4f7f77cc657783f45be9db7154d4
Alex Miller <apmiller108@yahoo.com>
2024-01-10 21:24:02 -0600

    Extracts modal component

---
+++ app/views/components/modal_component.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class ModalComponent < ApplicationViewComponent
+  attr_reader :id, :size
+
+  def initialize(id:, size: nil)
+    @id = id
+    @size = size
+  end
+
+  def bs_size_class
+    return '' if size.blank?
+
+    "modal-#{size}"
+  end
+
+  def body_turbo_frame_id
+    "#{id}-body"
+  end
+end
