6d0cf46ee01d761e5e78e59ba57a32d9bef898c5
Alex Miller <apmiller@covermymeds.com>
2024-01-12 17:20:50 -0600

    Moves transcription to view template

--- app/controllers/transcriptions_controller.rb
+++ app/controllers/transcriptions_controller.rb
@@ -1,16 +1,7 @@
 class TranscriptionsController < ApplicationController
   def index
-    transcription = current_user.transcriptions.find_by!(transcription_job_id: params[:transcription_job_id])
+    @transcription = current_user.transcriptions.find_by!(transcription_job_id: params[:transcription_job_id])
 
-    respond_to do |format|
-      format.turbo_stream do
-        render(
-          inline: turbo_stream.replace(
-            "transcription_job_#{transcription.transcription_job_id}",
-            TranscriptionComponent.new(transcription:).render_in(view_context)
-          )
-        )
-      end
-    end
+    respond_to(&:turbo_stream)
   end
 end
