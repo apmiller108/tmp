d8812e98eefd01092ea5ada542980384b4e3fc5b
Alex Miller <apmiller108@yahoo.com>
2024-01-11 18:33:23 -0600

    Animates modal body

--- app/views/components/modal_component/modal_controller.js
+++ app/views/components/modal_component/modal_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
+import autoAnimate from '@formkit/auto-animate'
 
 export default class extends Controller {
   static targets = ['title', 'bodyTurboFrame']
+
+  connect() {
+    autoAnimate(this.bodyTurboFrameTarget)
+  }
 
   onOpenModal({ detail: { src, title }}) {
     this.titleTarget.textContent = title
