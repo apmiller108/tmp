0760cd2f706074d205dc277dd1b78a3c10bca54e
Alex Miller <apmiller108@yahoo.com>
2024-01-15 16:46:16 -0600

    Renames variable

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,7 +1,7 @@
 class MemosController < ApplicationController
   def index
-    @memos = current_user.memos.with_rich_text_content_and_embeds.order(created_at: :desc)
-    @memos, @cursor = Paginate.call(relation: @memos, limit: 21, cursor: params[:c])
+    relation = current_user.memos.with_rich_text_content_and_embeds.order(created_at: :desc)
+    @memos, @cursor = Paginate.call(relation:, limit: 21, cursor: params[:c])
     respond_to do |format|
       format.html
       format.turbo_stream
