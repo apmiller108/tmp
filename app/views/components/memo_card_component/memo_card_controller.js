1cfe15050af69d5fc386ec5f9ba19f67ac8bae1d
Alex Miller <apmiller108@yahoo.com>
2024-01-17 21:25:09 -0600

    Catch tooltip instantiation error

--- app/views/components/memo_card_component/memo_card_controller.js
+++ app/views/components/memo_card_component/memo_card_controller.js
@@ -6,6 +6,18 @@ export default class extends Controller {
   toolTippable = [this.cardTextTarget]
 
   connect() {
-    this.toolTippable = this.toolTippable.map(elem => new bootstrap.Tooltip(elem))
+    try {
+      this.toolTippable = this.toolTippable.map(elem => new bootstrap.Tooltip(elem))
+    } catch (error) {
+      if (error.message === 'this._config is undefined') {
+        // this error happens when a new controller is instantiated (eg after a
+        // successful memo update) and tooltips are created again for elements
+        // that already have tooltips from the previous controller's
+        // instantiation.
+        return;
+      } else {
+        throw error
+      }
+    }
   }
 }
