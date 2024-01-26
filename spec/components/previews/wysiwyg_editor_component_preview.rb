aaf123d825d41ce6566f2908ad70221885133688
Alex Miller <apmiller@covermymeds.com>
2024-01-26 13:15:21 -0600

    Adds component preview

--- spec/components/previews/wysiwyg_editor_component_preview.rb
+++ spec/components/previews/wysiwyg_editor_component_preview.rb
@@ -1,5 +1,14 @@
 class WysiwygEditorComponentPreview < ViewComponent::Preview
-  def default
-    # render WysiwygEditorComponent.new(form:, rich_text_field: 'content')
+
+  # rails/view_components/wysiwyg_editor_component/new
+  def new
+    render WysiwygEditorComponent.new(object: Memo.new, method: 'content')
+  end
+
+  # rails/view_components/wysiwyg_editor_component/edit
+  def edit
+    memo = Memo.last
+    raise 'No memos found. See at least one memo to view the edit version' if memo.blank?
+    render WysiwygEditorComponent.new(object: memo, method: 'content')
   end
 end
