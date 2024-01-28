9ade9532133905c53164362ce2c996bdce2d11a9
Alex Miller <apmiller108@yahoo.com>
2024-01-28 00:03:04 -0600

    Adds align and input name options

--- app/views/components/color_picker_component.rb
+++ app/views/components/color_picker_component.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
 class ColorPickerComponent < ApplicationViewComponent
-  attr_reader :swatches, :default_color
+  attr_reader :swatches, :default_color, :opts
 
-  def initialize(swatches:, default_color:)
+  def initialize(swatches:, default_color:, **opts)
     @swatches = swatches
     @default_color = default_color
+    @opts = opts
+  end
+
+  def align = opts.fetch(:align, :center)
+  def input_name = opts.fetch(:input_name, :color)
+
+  def swatch_styles
+    {
+      left: 'left: -13rem;',
+      center: 'left: -6.5rem;'
+    }.fetch(align, '')
   end
 end
