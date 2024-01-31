4afa9d7c0e7ee2f644a805f941abf753cb15cebe
Alex Miller <apmiller108@yahoo.com>
2024-01-30 21:16:10 -0600

    Add color to memo form

--- app/views/components/memo_form_component.rb
+++ app/views/components/memo_form_component.rb
@@ -3,6 +3,9 @@
 class MemoFormComponent < ApplicationViewComponent
   attr_reader :memo
 
+  delegate :color, to: :memo
+  alias default_color color
+
   def initialize(memo:)
     @memo = memo
   end
@@ -19,14 +22,25 @@ class MemoFormComponent < ApplicationViewComponent
     memo.class::SWATCHES
   end
 
-  def default_color
-    memo.class::SWATCHES[:swatch1].last
-  end
-
   def color_picker_options
     {
       align: :left,
       input_name: "#{memo.model_name.element}[color]"
     }
+  end
+
+  def color_style
+    return unless color.present?
+
+    "box-shadow: 0 0 0.5rem 0.5rem rgba(#{rgb}, 0.5); border: 0.25rem solid rgba(#{rgb}, 0.8);"
+  end
+
+  private
+
+  def rgb
+    r = color[0..1].to_i(16)
+    g = color[2..3].to_i(16)
+    b = color[4..5].to_i(16)
+    [r, g, b].join(',')
   end
 end
--- app/views/components/memo_form_component/memo_form_controller.js
+++ app/views/components/memo_form_component/memo_form_controller.js
@@ -5,4 +5,13 @@ export default class extends Controller {
     await this.dispatch('memoFormSubmit')
     this.element.requestSubmit()
   }
+  onColorChosen(e) {
+    const hexColor = e.detail.color
+    const r = parseInt(hexColor.substring(0, 2), 16);
+    const g = parseInt(hexColor.substring(2, 4), 16);
+    const b = parseInt(hexColor.substring(4, 6), 16);
+    const rgb = [r, g, b].join(',')
+    this.element.style.border = `0.25rem solid rgba(${rgb}, 0.8)`
+    this.element.style.boxShadow = `0 0 0.5rem 0.5rem rgba(${rgb}, 0.5)`
+  }
 }
