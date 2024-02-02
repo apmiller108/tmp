759d9b0640d2fe497fb06c83abd03702a999785a
Alex Miller <apmiller108@yahoo.com>
2024-02-01 21:28:02 -0600

    Extracts color to value object

---
+++ lib/color.rb
@@ -0,0 +1,25 @@
+class Color
+  DEFAULT = 'e4f2fe'.freeze
+
+  attr_reader :hex
+
+  def initialize(hex = nil)
+    @hex = hex || DEFAULT
+  end
+
+  def to_rgb
+    [r, g, b]
+  end
+
+  def r = hex[0..1].to_i(16)
+  def g = hex[2..3].to_i(16)
+  def b = hex[4..5].to_i(16)
+
+  def darkish?
+    rgb.sum < (255 * 3) / 3.7
+  end
+
+  def default?
+    hex == DEFAULT
+  end
+end
