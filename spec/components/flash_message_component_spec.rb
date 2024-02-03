1a819d0e093a5f61e03c28a7e5dd4381a1085792
Alex Miller <apmiller108@yahoo.com>
2024-02-02 19:34:42 -0600

    Update flash message spec

--- spec/components/flash_message_component_spec.rb
+++ spec/components/flash_message_component_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe FlashMessageComponent, type: :component do
-  let(:alert) { nil }
-  let(:notice) { nil }
-  let(:flash) { instance_double ActionDispatch::Flash::FlashHash, alert:, notice: }
+  subject { page }
+
+  let(:flash) { ActionDispatch::Flash::FlashHash.new }
   let(:validation_errors) { ['Error 1', 'Error 2'] }
   let(:errors) { instance_double(ActiveModel::Errors, full_messages: validation_errors) }
   let(:record) { double }
@@ -11,53 +11,77 @@ RSpec.describe FlashMessageComponent, type: :component do
   context 'when flash has notice' do
     let(:notice) { 'This is a notice message.' }
 
-    it 'renders an alert-info message' do
+    before do
+      flash.notice = notice
       render_inline(described_class.new(flash:))
+    end
 
+    it 'renders an alert-info message' do
       expect(page).to have_css('.alert-info', text: 'This is a notice message.')
     end
+
+    it { is_expected.to have_css 'i.bi-info-square' }
   end
 
   context 'when flash has alert' do
     let(:alert) { 'This is an alert message.' }
+
+    before do
+      flash.alert = alert
+    end
 
     it 'renders an alert-warning message' do
       render_inline(described_class.new(flash:))
 
       expect(page).to have_css('.alert-warning', text: 'This is an alert message.')
     end
-  end
 
-  context 'when record has validation errors' do
-    let(:alert) { 'This is an alert message.' }
+    it 'has the proper icon' do
+      render_inline(described_class.new(flash:))
 
-    before do
-      allow(record).to receive(:errors).and_return(errors)
-      allow(errors).to receive(:any?).and_return(true)
+      expect(page).to have_css 'i.bi-exclamation-triangle'
     end
 
-    it 'renders validation error messages' do
-      render_inline(described_class.new(flash:, record:))
+    context 'when record has validation errors' do
+      before do
+        allow(record).to receive(:errors).and_return(errors)
+        allow(errors).to receive(:any?).and_return(true)
+      end
 
-      validation_errors.each do |e|
-        expect(page).to have_css('.list-group-item-warning', text: e)
+      it 'renders validation error messages' do
+        render_inline(described_class.new(flash:, record:))
+
+        validation_errors.each do |e|
+          expect(page).to have_css('p', text: e)
+        end
+      end
+    end
+
+    context 'when record does not have validation errors' do
+      let(:validation_errors) { [] }
+
+      before do
+        allow(record).to receive(:errors).and_return(errors)
+        allow(errors).to receive(:any?).and_return(false)
+      end
+
+      it 'does not render validation error messages' do
+        render_inline(described_class.new(flash:, record:))
+
+        expect(page).not_to have_css('.list-group-item-warning')
       end
     end
   end
 
-  context 'when record does not have validation errors' do
-    let(:alert) { 'This is an alert message.' }
-    let(:validation_errors) { [] }
+  context 'when the flash is a success' do
+    let(:message) { 'This is a success message' }
 
     before do
-      allow(record).to receive(:errors).and_return(errors)
-      allow(errors).to receive(:any?).and_return(false)
+      flash[:success] = message
+      render_inline(described_class.new(flash:))
     end
 
-    it 'does not render validation error messages' do
-      render_inline(described_class.new(flash:, record:))
-
-      expect(page).not_to have_css('.list-group-item-warning')
-    end
+    it { is_expected.to have_content message }
+    it { is_expected.to have_css 'i.bi-check-circle' }
   end
 end
