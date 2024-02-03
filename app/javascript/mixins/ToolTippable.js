1e61a38d6a5fe2ff7c50b1cb62a857e7966124ee
Alex Miller <apmiller108@yahoo.com>
2024-02-03 16:10:47 -0600

    Adds tooltip mixin

--- app/javascript/mixins/ToolTippable.js
+++ app/javascript/mixins/ToolTippable.js
@@ -2,8 +2,19 @@ import { Tooltip } from 'bootstrap'
 
 export default {
   connect() {
-    console.log('connect')
-    this.toolTippable = Array.from(this.element.querySelectorAll('[data-bs-toggle="tooltip"]')).map(e => new Tooltip(e))
+    try {
+      this.toolTippable = Array.from(this.element.querySelectorAll('[data-bs-toggle="tooltip"]')).map(e => new Tooltip(e))
+    } catch (error) {
+      if (error.stack.includes("_Tooltip._setListeners")) {
+        // this error happens when a new controller is instantiated (eg after a
+        // successful memo update) and tooltips are created again for elements
+        // that already have tooltips from the previous controller's
+        // instantiation.
+        return;
+      } else {
+        throw error
+      }
+    }
   },
 
   disconnect() {
--- app/views/components/memo_card_component/memo_card_controller.js
+++ app/views/components/memo_card_component/memo_card_controller.js
@@ -1,23 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 import { Tooltip } from "bootstrap"
+import ToolTippable from '@javascript/mixins/ToolTippable'
 
-export default class extends Controller {
-  static targets = ['cardText']
-  toolTippable = [this.cardTextTarget]
-
-  connect() {
-    try {
-      this.toolTippable = this.toolTippable.map(elem => new Tooltip(elem))
-    } catch (error) {
-      if (error.stack.includes("_Tooltip._setListeners")) {
-        // this error happens when a new controller is instantiated (eg after a
-        // successful memo update) and tooltips are created again for elements
-        // that already have tooltips from the previous controller's
-        // instantiation.
-        return;
-      } else {
-        throw error
-      }
-    }
-  }
+export default class MemoCardController extends Controller {
 }
+
+Object.assign(MemoCardController.prototype, ToolTippable)
+
+export default MemoCardController
