97821e344d99be1ba2b801b6462bc0d8fc25c416
Alex Miller <apmiller108@yahoo.com>
2024-02-04 13:52:39 -0600

    Adds option chaining for missing meta tags in test env

--- app/javascript/http.js
+++ app/javascript/http.js
@@ -7,7 +7,7 @@ export const generateText = ({ input, text_id }) => {
   const headers = {
     'Content-Type': 'application/json',
     Accept: 'text/vnd.turbo-stream.html',
-    'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
+    'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]')?.content
   }
 
   return fetch('/generative_texts', {
