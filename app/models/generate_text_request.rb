a57fbc240ef6d096860d6aeed317287e8995cc56
Alex Miller <apmiller108@yahoo.com>
2024-02-11 13:06:14 -0600

    Adds length validation

--- app/models/generate_text_request.rb
+++ app/models/generate_text_request.rb
@@ -1,5 +1,6 @@
 class GenerateTextRequest < ApplicationRecord
   belongs_to :user
 
-  validates :text_id, :prompt, presence: true
+  validates :text_id, presence: true, length: { maximum: 50 }
+  validates :prompt, presence: true, length: { maximum: 8000 }
 end
