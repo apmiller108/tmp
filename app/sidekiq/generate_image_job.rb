7e9aeb63b977eefdd0641a07ae2442b2b1e17cc2
Alex Miller <apmiller108@yahoo.com>
2024-02-13 21:41:35 -0600

    Adds generate image job

---
+++ app/sidekiq/generate_image_job.rb
@@ -0,0 +1,37 @@
+class GenerateImageJob
+  include Sidekiq::Job
+  include Flashable
+
+  sidekiq_options retry: false
+
+  def perform(generate_image_request_id)
+    request = GenerateImageRequest.find(generate_image_request_id)
+    image = generate_image(request.parameterize)
+
+    payload = { generate_image: { image_id: request.image_id, image:, error: nil } }
+
+    if image
+      MyChannel.broadcast_to(request.user, payload)
+    else
+      payload[:error] = true
+      MyChannel.broadcast_to(request.user, payload)
+      broadcast_flash(request.user)
+    end
+  end
+
+  private
+
+  def generate_image(params)
+    GenerativeImage.new.text_to_image(params)
+  rescue StandardError => e
+    Rails.logger.warn("#{self.class}: #{e} : #{e.cause}")
+    false
+  end
+
+  def broadcast_flash(user)
+    flash.alert = I18n.t('unable_to_generate_text')
+    ViewComponentBroadcaster.call([user, TurboStreams::STREAMS[:memos]],
+                                  component: FlashMessageComponent.new(flash:),
+                                  action: :replace)
+  end
+end
