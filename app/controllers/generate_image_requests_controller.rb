eceaf8a440ba925232832c009f5924062ce29099
Alex Miller <apmiller@covermymeds.com>
2024-02-15 14:45:14 -0600

    Enqueues job

--- app/controllers/generate_image_requests_controller.rb
+++ app/controllers/generate_image_requests_controller.rb
@@ -5,7 +5,7 @@ class GenerateImageRequestsController < ApplicationController
     respond_to do |format|
       format.turbo_stream do
         if form.submit
-          # GenerateImageJob.perform_async(form.generate_text_request.id)
+          GenerateImageJob.perform_async(form.generate_text_request.id)
           head :created
         else
           flash.now.alert = t('unable_to_generate_image')
