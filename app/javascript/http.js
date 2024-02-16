cfb50aa734a7a9677414197ddf074f4178473706
Alex Miller <apmiller@covermymeds.com>
2024-02-16 15:40:29 -0600

    Adds function to post generate image request

--- app/javascript/http.js
+++ app/javascript/http.js
@@ -1,18 +1,46 @@
+function getCsrfToken() {
+  return document.querySelector('meta[name="csrf-token"]')?.content
+}
+
 export const generateText = ({ prompt, text_id }) => {
   const body = JSON.stringify({
     generate_text_request: {
       prompt, text_id
     }
-  });
+  })
   const headers = {
     'Content-Type': 'application/json',
     Accept: 'text/vnd.turbo-stream.html',
-    'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]')?.content
+    'X-CSRF-Token': getCsrfToken()
   }
 
   return fetch('/generate_text_requests', {
     method: 'POST',
-    body,
-    headers
+    headers,
+    body
   })
+}
+
+export const generateImage = ({ prompts, image_id, style, dimensions }) => {
+  const body = JSON.stringify({
+    generate_image_request: {
+      prompts, image_id, style, dimensions
+    }
+  })
+  const headers = {
+    'Content-Type': 'application/json',
+    Accept: 'text/vnd.turbo-stream.html',
+    'X-CSRF-Token': getCsrfToken()
+  }
+
+  return fetch('/generate_image_requests', {
+    method: 'POST',
+    headers,
+    body
+  })
+}
+
+export default {
+  generateText,
+  generateImage
 }
