2e194f565f29fcfcf7a0672331acf10e5edaa485
Alex Miller <apmiller@covermymeds.com>
2024-03-06 15:10:06 -0600

    Scope blob to current user via gen image request

--- app/controllers/blob_details_controller.rb
+++ app/controllers/blob_details_controller.rb
@@ -1,6 +1,8 @@
 class BlobDetailsController < ApplicationController
   def show
-    @blob = ActiveStorage::Blob.find(params[:active_storage_blob_id])
+    @blob = ActiveStorage::Blob.joins(:generate_image_request)
+                               .where(generate_image_request: { user_id: current_user.id })
+                               .find(params[:active_storage_blob_id])
     respond_to(&:html)
   end
 end
