7060dbbe6f799c4b395caedc32f1a07f881a5711
Alex Miller <apmiller108@yahoo.com>
2024-03-12 22:09:47 -0500

    Adds system test

--- spec/components/clipboard_component_spec.rb
+++ spec/components/clipboard_component_spec.rb
@@ -4,10 +4,16 @@ RSpec.describe ClipboardComponent, type: :component do
   subject { page }
 
   before do
-    render_inline(described_class.new('my-clipboard') { |c| c.with_copyable { '<input type="text"></input>'} })
+    render_inline(
+      described_class.new('my-clipboard').tap do |c|
+        c.with_copyable { '<input type="text" name="copyable"></input>'.html_safe }
+      end
+    )
   end
 
-  it { is_expected.to have_css '.c-clipboard.my-clipboard[data-controller="clipboard"]'}
+  it { is_expected.to have_css '.c-clipboard.my-clipboard[data-controller="clipboard"]' }
   it { is_expected.to have_css 'button.copy-btn' }
-  it { is_expected.to have_css 'input[type="text"]' }
+  it { is_expected.to have_css 'input[name="copyable"]' }
+  it { is_expected.to have_css '.bi.bi-copy[data-clipboard-target="copyIcon"]'}
+  it { is_expected.to have_css '.bi.bi-check2-square[data-clipboard-target="successIcon"]' }
 end
--- spec/system/components/clipboard_component_spec.rb
+++ spec/system/components/clipboard_component_spec.rb
@@ -1,16 +1,33 @@
 require 'system_helper'
 
 RSpec.describe 'ClipboardComponent', type: :system do
+  before do
+    # Mock the clipboard API. For some reason, it is not defined. Maybe because its headless?
+    # See also https://github.com/rubycdp/ferrum?tab=readme-ov-file#evaluate_asyncexpression-wait_time-args
+    page.driver.browser.evaluate_on_new_document(<<~JS)
+      const clipboard = {
+        writeText: text => new Promise(resolve => this.text = text),
+        readText: () => new Promise(resolve => resolve(this.text))
+      }
+      Object.defineProperty(navigator, 'clipboard', { value: clipboard } )
+    JS
+  end
+
   specify 'copying text to the clipboard' do
     visit ClipboardComponentPreview::DEFAULT_PATH
 
     expect(page).to have_css 'i.bi-copy', visible: true
     expect(page).to have_css 'i.bi-check2-square', visible: false
 
-    fill_in 'copyable', with: 'asdf'
+    input = 'asdf'
+    fill_in 'copyable', with: input
     find('.copy-btn').click
 
     expect(page).to have_css 'i.bi-copy', visible: false
     expect(page).to have_css 'i.bi-check2-square', visible: true
+
+    # See also https://github.com/rubycdp/ferrum?tab=readme-ov-file#evaluate_asyncexpression-wait_time-args
+    text = page.driver.browser.evaluate_async(%(arguments[0](navigator.clipboard.readText())), 1)
+    expect(text).to eq input
   end
 end
