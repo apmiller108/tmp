82b3aa8a066f8918b1e7313346c49b6a3c535634
Alex Miller <apmiller@covermymeds.com>
2024-03-13 14:21:22 -0500

    Fixes preview

--- spec/components/previews/color_picker_component_preview.rb
+++ spec/components/previews/color_picker_component_preview.rb
@@ -1,6 +1,6 @@
 class ColorPickerComponentPreview < ViewComponent::Preview
   DEFAULT_PATH = 'rails/view_components/color_picker_component/default'.freeze
   def default
-    render ColorPickerComponent.new(swatches: Memo::SWATCHES, selected_color: nil)
+    render ColorPickerComponent.new(swatches: Memo::SWATCHES, selected_color: nil, align: :end)
   end
 end
