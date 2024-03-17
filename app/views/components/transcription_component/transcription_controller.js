3a9b17e0c96bfad56293c0c098a1990d3cc39234
Alex Miller <apmiller108@yahoo.com>
2024-03-17 17:42:54 -0500

    Updates styles

--- app/views/components/transcription_component/transcription_controller.js
+++ app/views/components/transcription_component/transcription_controller.js
@@ -3,6 +3,7 @@ import autoAnimate from '@formkit/auto-animate'
 
 export default class extends Controller {
   static targets = ['transcriptionSummaryTab']
+
   connect() {
     if (this.hasTranscriptionSummaryTabTarget) {
       autoAnimate(this.transcriptionSummaryTabTarget)
