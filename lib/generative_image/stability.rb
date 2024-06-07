dfada2ef0b42380d2e27a06abc75591fd402d5f7
Alex Miller <apmiller@covermymeds.com>
2024-06-07 15:22:35 -0500

    Removes unused constants

--- lib/generative_image/stability.rb
+++ lib/generative_image/stability.rb
@@ -6,20 +6,7 @@ class GenerativeImage
 
     HOST = 'https://api.stability.ai'.freeze
     ENGINES_ENDPOINT = '/v1/engines/list'.freeze
-    GENERATION_ENDPOINT = '/v1/generation/%<engine>s/text-to-image'.freeze
     CORE_GENERATION_ENDPOINT = '/v2beta/stable-image/generate/core'.freeze
-
-    ENGINES = {
-      v1_6: {
-        id: 'stable-diffusion-v1-6', dimensions: %w[320x320 512x512 768x768]
-      },
-      xl_v1: {
-        id: 'stable-diffusion-xl-1024-v1-0',
-        dimensions: %w[1024x1024 1152x896 1216x832 1344x768 1536x640 640x1536 768x1344 832x1216 896x1152]
-      }
-    }.freeze
-
-    DIMENSIONS = ENGINES.values.flat_map { |e| e[:dimensions] }
 
     STYLE_PRESETS = %w[
       3d-model
