65bac08b7e6f2982b711974efed4141dd7f66d94
Alex Miller <apmiller@covermymeds.com>
2024-06-17 15:51:42 -0500

    Adpots patch to fix action text attachments

--- config/initializers/action_text_patch.rb
+++ config/initializers/action_text_patch.rb
@@ -1,14 +1,12 @@
 module ActionText
   class Content
+    # Update to 7.1.3.4 prevents images from being rendered (and probably more)
+    # See https://github.com/rails/rails/issues/52127
     def render_attachments(**options, &block)
       content = fragment.replace(ActionText::Attachment.tag_name) do |node|
-        puts '*' * 30
-        p node
-        # p node["content"]
-        # p sanitize_content_attachment(node["content"])
-        node["content"] = sanitize_content_attachment(node["content"])
-        p node
-        node.delete("content")
+        if node.key? 'content'
+          node['content'] = sanitize_content_attachment(node['content'])
+        end
         block.call(attachment_for_node(node, **options))
       end
       self.class.new(content, canonicalize: false)
