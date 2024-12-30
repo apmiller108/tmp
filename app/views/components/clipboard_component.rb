e7fd7e31a3715908a16d2da397a9f348d888a922
Alex Miller <apmiller108@yahoo.com>
2024-12-29 19:37:06 -0600

    Adds copy component to assistant response

--- app/views/components/clipboard_component.rb
+++ app/views/components/clipboard_component.rb
@@ -3,9 +3,22 @@
 class ClipboardComponent < ApplicationViewComponent
   renders_one :copyable
 
-  attr_reader :css_class
+  attr_reader :css_class, :position
 
-  def initialize(css_class = 'clipboard')
+  def initialize(css_class: 'clipboard', position: :top)
     @css_class = css_class
+    @position = position
+  end
+
+  def tip
+    t('copy')
+  end
+
+  def top?
+    position == :top
+  end
+
+  def bottom?
+    position == :bottom
   end
 end
--- spec/components/previews/clipboard_component_preview.rb
+++ spec/components/previews/clipboard_component_preview.rb
@@ -1,7 +1,7 @@
 class ClipboardComponentPreview < ViewComponent::Preview
   DEFAULT_PATH = 'rails/view_components/clipboard_component/default'.freeze
   def default
-    render ClipboardComponent.new('my-clipboard') do |c|
+    render ClipboardComponent.new(css_class: 'my-clipboard') do |c|
       c.with_copyable do
         tag.input(type: :text, id: :copyable, data: { 'clipboard-target' => 'source' })
       end
