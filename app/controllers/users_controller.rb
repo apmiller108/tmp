d91e95ca134b00caadd680a182806da2a8b4081d
Alex Miller <apmiller@covermymeds.com>
2024-12-31 15:12:24 -0600

    Cleans up users view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     respond_to do |format|
       format.html do
-        render UserComponent.new(user: current_user).with_content('Yo!'), content_type: 'text/html'
+        render UserComponent.new(user: current_user), content_type: 'text/html'
       end
       format.json { render json: current_user.as_json(only: %i[id email]) }
     end
