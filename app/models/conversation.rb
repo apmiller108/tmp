c9853dac02a3307bf3333ac320e3d339ef89e866
Alex Miller <apmiller@covermymeds.com>
2025-01-02 21:44:31 -0600

    Sets title from first prompt

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -7,19 +7,21 @@ class Conversation < ApplicationRecord
 
   has_many :generate_text_requests, dependent: :nullify
 
+  validates :title, presence: true, length: { maximum: 100 }
+
+  before_validation :set_title_from_prompt, on: :create
+
   attribute :exchange, default: []
 
   delegate :first, to: :exchange
 
-  # TODO: custom validator exchange JSON schema
-
-  def starter
-    return if first.nil?
-
-    first.fetch('content').first.fetch('text')
-  end
-
   def turns
     @turns ||= exchange.map { |e| Turn.new(e) }
+  end
+
+  private
+
+  def set_title_from_prompt
+    self.title = turns.first.content.truncate(35) if title.blank?
   end
 end
