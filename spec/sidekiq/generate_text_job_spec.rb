2d00329a8399c0f4be00ec5cc136fe98c0e1b953
Alex Miller <apmiller108@yahoo.com>
2025-01-13 21:42:01 -0600

    Updates spec

--- spec/sidekiq/generate_text_job_spec.rb
+++ spec/sidekiq/generate_text_job_spec.rb
@@ -4,17 +4,22 @@ RSpec.describe GenerateTextJob, type: :job do
   describe 'sidekiq_options' do
     subject { described_class.sidekiq_options }
 
-    it { is_expected.to include('retry' => false) }
+    it { is_expected.to include('retry' => 1) }
   end
 
   describe '#perform' do
     subject(:perform) { described_class.new.perform(generate_text_request.id) }
 
     let(:generate_text_request) { build_stubbed :generate_text_request, :with_generate_text_preset, conversation: }
+    let(:user) { generate_text_request.user }
     let(:conversation) { build_stubbed :conversation }
+    let(:response_data) { { 'content' => 'response data' } }
     let(:response) do
-      instance_double(GenerativeText::AWS::Client::InvokeModelResponse, content: Faker::Lorem.paragraph)
+      instance_double(GenerativeText::Anthropic::InvokeModelResponse,
+                      content: Faker::Lorem.paragraph, data: response_data)
     end
+    let(:prompt_form_component) { instance_double PromptFormComponent }
+    let(:conversation_turn_component) { instance_double ConversationTurnComponent }
     let(:generative_text) { instance_double(GenerativeText, invoke_model: response) }
 
     before do
@@ -22,50 +27,84 @@ RSpec.describe GenerateTextJob, type: :job do
       allow(MyChannel).to receive(:broadcast_to)
       allow(ViewComponentBroadcaster).to receive(:call)
       allow(GenerativeText).to receive(:new).and_return(generative_text)
+      allow(PromptFormComponent).to receive(:new).with(conversation:).and_return(prompt_form_component)
+      allow(ConversationTurnComponent).to receive(:new).with(generate_text_request:)
+                                                       .and_return(conversation_turn_component)
+      allow(generate_text_request).to receive(:update!)
+      allow(generate_text_request).to receive(:in_progress!)
+      allow(generate_text_request).to receive(:failed!)
+      allow(conversation).to receive(:reload).and_return(conversation)
     end
 
     context 'when the text is generated successfully' do
-      it 'invokes the model with the proper params' do
+      it 'marks the request as in_progress' do
         perform
-        expect(generative_text).to have_received(:invoke_model)
-          .with(prompt: generate_text_request.prompt,
-                temperature: generate_text_request.temperature,
-                model: generate_text_request.model,
-                messages: conversation.exchange,
-                system_message: generate_text_request.system_message,
-                max_tokens: 500)
+        expect(generate_text_request).to have_received(:in_progress!)
       end
 
       it 'broadcasts the text content' do
         perform
-        expect(MyChannel).to have_received(:broadcast_to).with(generate_text_request.user,
-                                                               generate_text: { text_id: generate_text_request.text_id,
-                                                                                content: response.content, error: nil })
+        expect(MyChannel).to(
+          have_received(:broadcast_to).with(
+            user,
+            generate_text: { 'text_id' => generate_text_request.text_id,
+                             'conversation_id' => conversation.id,
+                             'user_id' => user.id,
+                             content: response.content,
+                             error: nil }
+          )
+        )
+      end
+
+      it 'updates the request' do
+        perform
+        expect(generate_text_request).to have_received(:update!).with(response: response_data, status: 'completed')
+      end
+
+      it 'broadcasts the ConversationTurnComponent' do
+        perform
+        expect(ViewComponentBroadcaster).to(
+          have_received(:call).with([user, TurboStreams::STREAMS[:main]],
+                                    component: conversation_turn_component, action: :replace)
+        )
+      end
+
+      it 'broadcasts the PromptFormComponent' do
+        perform
+        expect(ViewComponentBroadcaster).to(
+          have_received(:call).with([user, TurboStreams::STREAMS[:main]],
+                                    component: prompt_form_component, action: :replace)
+        )
       end
     end
 
-    context 'when the text generation fails' do
-      before do
-        allow(generative_text).to receive(:invoke_model).and_raise(GenerativeText::InvalidRequestError)
-        allow(Rails.logger).to receive(:warn)
+    describe '.on_retries_exhausted' do
+      subject(:on_retries_exhausted) { described_class.on_retries_exhausted(generate_text_request.id) }
+
+      it 'marks the request as failed' do
+        on_retries_exhausted
+        expect(generate_text_request).to have_received(:failed!)
       end
 
-      it 'logs the error' do
-        perform
-        expect(Rails.logger).to have_received(:warn).with('GenerateTextJob: GenerativeText::InvalidRequestError : ')
+      it 'broadcasts the ConversationTurnComponent' do
+        on_retries_exhausted
+        expect(ViewComponentBroadcaster).to(
+          have_received(:call).with([user, TurboStreams::STREAMS[:main]],
+                                    component: conversation_turn_component, action: :replace)
+        )
       end
 
       it 'broadcasts an error response' do
-        perform
-        expect(MyChannel).to have_received(:broadcast_to).with(generate_text_request.user,
+        on_retries_exhausted
+        expect(MyChannel).to have_received(:broadcast_to).with(user,
                                                                generate_text: { text_id: generate_text_request.text_id,
                                                                                 content: nil, error: true })
       end
 
       it 'broadcasts a flash message' do
-        perform
+        on_retries_exhausted
         expect(ViewComponentBroadcaster).to(
-          have_received(:call).with([generate_text_request.user, TurboStreams::STREAMS[:memos]],
+          have_received(:call).with([generate_text_request.user, TurboStreams::STREAMS[:main]],
                                     component: kind_of(FlashMessageComponent), action: :update)
         )
       end
