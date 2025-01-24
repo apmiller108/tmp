7c406d217d65d20addcee19173c5989f0bae2b8f
Alex Miller <apmiller108@yahoo.com>
2025-01-23 21:33:29 -0600

    Updates todos

--- app/views/components/file_input_component/file_input_controller.js
+++ app/views/components/file_input_component/file_input_controller.js
@@ -4,19 +4,16 @@ export default class FileInputController extends Controller {
   static targets = ['input', 'uploadProgress']
 
   onUploadStart() {
-    console.log('start')
     this.uploadProgressTarget.classList.remove('.error')
     this.uploadProgressTarget.style.zIndex = '2'
   }
 
   onUploadProgress(e) {
     const { progress } = event.detail
-    console.log(`progres: ${progress}`)
     this.uploadProgressTarget.style.width = `${progress}%`
   }
 
   onUploadEnd() {
-    console.log('end')
     this.uploadProgressTarget.style.zIndex = ''
     this.uploadProgressTarget.style.width = '0'
   }
