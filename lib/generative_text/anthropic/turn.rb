c7f86827dab7bbd801199532f1dbb1689874fcbd
Alex Miller <apmiller108@yahoo.com>
2025-02-09 17:53:44 -0600

    Refactors turn

--- lib/generative_text/anthropic/turn.rb
+++ lib/generative_text/anthropic/turn.rb
@@ -1,8 +1,6 @@
 class GenerativeText
   module Anthropic
-    module Turn
-      module_function
-
+    class Turn
       ASSISTANT = 'assistant'.freeze
       USER = 'user'.freeze
 
@@ -11,10 +9,28 @@ class GenerativeText
       # @param [GenerateTextRequest] generate_text_request
       # @param [Array<ConversationTurn>]
       # @return [Array<Hash>]
-      def for(generate_text_request, turns:)
+      def self.for(generate_text_request, turns: [])
+        new(generate_text_request:, turns:).turn
+      end
+
+      def self.user_turn(generate_text_request, turns: [])
+        new(generate_text_request:, turns:).user_turn
+      end
+
+      attr_reader :generate_text_request, :turns
+
+      delegate :prompt, :response_content, to: :generate_text_request
+      alias assistant_content response_content
+
+      def initialize(generate_text_request:, turns:)
+        @generate_text_request = generate_text_request
+        @turns = turns
+      end
+
+      def turn
         [
-          user_turn(generate_text_request, turns:),
-          { 'role' => ASSISTANT, 'content' => generate_text_request.response_content || 'no content' }
+          user_turn,
+          assistant_turn
         ]
       end
 
@@ -22,36 +38,66 @@ class GenerativeText
       # several hashes: text, images, pdfs, etc... When the previous turn is an
       # image request, include it in the next user turn so the LLM will have the
       # image in it's context window.
-      def user_turn(generate_text_request, turns:)
-        turn_index = turns.find_index { _1.turnable_id == generate_text_request.id }
-        previous_turn = turn_index.positive? ? turns[turn_index - 1] : nil
+      def user_turn
         {
           'role' => USER,
-          'content' => [
-            { 'text' => generate_text_request.prompt, 'type' => 'text' }
-          ].tap do |content|
-            if generate_text_request.image_attached?
-              add_user_image_content(content:, image: generate_text_request.file.variant(:webp))
-            end
-            if previous_turn.present? && previous_turn.generated_image?
-              add_user_image_content(content:, image: previous_turn.turnable.image.variant(:webp))
-            end
-          end
+          'content' => user_content
         }
       end
 
-      def add_user_image_content(content:, image:)
-        content.prepend(
-          {
-            'type' => 'image',
-            'source' => {
-              'type' => 'base64',
-              'media_type' => image.content_type,
-              'data' => BlobEncoder.encode64(image.blob)
-            },
-            'cache_control' => { 'type' => 'ephemeral' }
-          }
-        )
+      def assistant_turn
+        {
+          'role' => ASSISTANT,
+          'content' => assistant_content || 'no content'
+        }
+      end
+
+      private
+
+      def user_content
+        [
+          user_generate_image_content,
+          user_upload_image_content,
+          user_text_content
+        ].compact
+      end
+
+      def user_text_content
+        { 'text' => prompt, 'type' => 'text' }
+      end
+
+      def user_upload_image_content
+        return unless generate_text_request.image_attached?
+
+        user_image_content(image: generate_text_request.file.variant(:webp).image)
+      end
+
+      def user_generate_image_content
+        return unless previous_turn.present? && previous_turn.generated_image?
+
+        user_image_content(image: previous_turn.turnable.image.variant(:webp).image)
+      end
+
+      def user_image_content(image:)
+        return unless generate_text_request.image_capable?
+
+        {
+          'type' => 'image',
+          'source' => {
+            'type' => 'base64',
+            'media_type' => image.content_type,
+            'data' => BlobEncoder.encode64(image)
+          },
+          'cache_control' => { 'type' => 'ephemeral' }
+        }
+      end
+
+      def previous_turn
+        turn_index.positive? ? turns[turn_index - 1] : nil
+      end
+
+      def turn_index
+        turns.find_index { _1.turnable_id == generate_text_request.id }
       end
     end
   end
