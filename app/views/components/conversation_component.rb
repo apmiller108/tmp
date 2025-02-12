7d327acdc7f46fd2646cc2b829ba62babd5288d3
Alex Miller <apmiller108@yahoo.com>
2025-02-11 21:29:30 -0600

    Improves centering of form on new conversation

--- app/views/components/conversation_component.rb
+++ app/views/components/conversation_component.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class ConversationComponent < ApplicationViewComponent
-  attr_reader :conversation_form
+  attr_reader :conversation_form, :opts
 
   delegate :conversation, to: :conversation_form
 
-  def initialize(conversation_form:)
+  def initialize(conversation_form:, **opts)
     @conversation_form = conversation_form
+    @opts = opts
   end
 
   def id
