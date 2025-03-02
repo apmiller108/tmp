f573b4bd1f5b5c69b21922b34f74b96eea71977a
Alex Miller <apmiller108@yahoo.com>
2025-03-02 17:26:19 -0600

    Wraps pre tags in clipboard

--- app/views/components/generate_text_request_component.rb
+++ app/views/components/generate_text_request_component.rb
@@ -17,13 +17,7 @@ class GenerateTextRequestComponent < ApplicationViewComponent
 
   # Heading anchor tags are removed as they will interfere with the Scrollspy
   def assistant_response
-    Commonmarker.to_html(
-      response.content,
-      options: {
-        parse: { smart: true }
-      },
-      plugins: { syntax_highlighter: { theme: 'Solarized (dark)' } }
-    ).gsub!(%r{<a href="#[^"]*" aria-hidden="true" class="anchor" id="[^"]*"></a>}, '')
+    formatted_assistant_response
   end
 
   def id
@@ -56,5 +50,47 @@ class GenerateTextRequestComponent < ApplicationViewComponent
       resize_to_limit: [100, 100],
       **ActiveStorage::Blob::WEBP_VARIANT_OPTS
     }
+  end
+
+  private
+
+  def assistant_response_html
+    Commonmarker.to_html(
+      response.content,
+      options: {
+        parse: { smart: true }
+      },
+      plugins: { syntax_highlighter: { theme: 'Solarized (dark)' } }
+    )
+  end
+
+  def formatted_assistant_response
+    doc = Nokogiri::HTML.fragment(assistant_response_html)
+
+    # Remove anchor tags
+    doc.css('a[aria-hidden="true"].anchor').each(&:remove)
+
+    # Wrap pre tags in a ClipboardComponent
+    doc.css('pre').each do |pre_tag|
+      pre_tag.replace(
+        Nokogiri::HTML.fragment(copyable_pre(pre_tag))
+      )
+    end
+
+    doc.to_html
+  end
+
+  def copyable_pre(pre_tag)
+    original_content = pre_tag.inner_html
+    attributes = pre_tag.attributes.transform_values(&:value)
+
+    # Create a new fragment with the rendered component
+    render ClipboardComponent.new(css_class: 'prompt', y: :top, x: :end) do |c|
+      c.with_copyable do
+        content_tag(:pre, original_content.html_safe,
+                    data: { 'clipboard-target' => 'source' },
+                    **attributes)
+      end
+    end
   end
 end
