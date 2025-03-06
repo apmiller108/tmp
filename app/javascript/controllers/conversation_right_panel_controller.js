40f3d26a19d7b7e5573fc8ccd8f8fb67ff89a1b9
Alex Miller <apmiller@covermymeds.com>
2025-03-06 09:57:52 -0600

    Fixes button spacing and adds scrollspy heading

--- app/javascript/controllers/conversation_right_panel_controller.js
+++ app/javascript/controllers/conversation_right_panel_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import autoAnimate from '@formkit/auto-animate'
 
 export default class ConversationRightPanel extends Controller {
-  static targets = ['collapseIcon', 'controls', 'scrollspy']
+  static targets = ['collapseIcon', 'controls', 'scrollspy', 'scrollspyHeading']
 
   connect() {
     autoAnimate(this.scrollspyTarget)
@@ -22,12 +22,14 @@ export default class ConversationRightPanel extends Controller {
 
   onHidePanel() {
     this.controlsTarget.classList.add('panel-hide')
+    this.scrollspyHeadingTarget.classList.add('d-none')
     this.collapseIconTarget.classList.remove('bi-arrows-collapse-vertical')
     this.collapseIconTarget.classList.add('bi-arrows-expand-vertical')
   }
 
   onShowPanel() {
     this.controlsTarget.classList.remove('panel-hide')
+    this.scrollspyHeadingTarget.classList.remove('d-none')
     this.collapseIconTarget.classList.remove('bi-arrows-expand-vertical')
     this.collapseIconTarget.classList.add('bi-arrows-collapse-vertical')
   }
