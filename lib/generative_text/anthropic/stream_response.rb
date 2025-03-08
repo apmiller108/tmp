092e1bfe31a87f05811166d622faac6f731e5153
Alex Miller <apmiller108@yahoo.com>
2025-03-07 22:03:05 -0600

    Adds stream response object

---
+++ lib/generative_text/anthropic/stream_response.rb
@@ -0,0 +1,50 @@
+class GenerativeText
+  module Anthropic
+    class StreamResponse
+      attr_reader :message, :content_blocks, :complete
+
+      def initialize
+        @message = {}
+        @content_blocks = []
+        @complete = false
+      end
+
+      # rubocop:disable Metrics/MethodLength
+      def update(event_type, data)
+        case event_type
+        when 'message_start'
+          @message = data.fetch('message')
+        when 'content_block_start'
+          index = data.fetch('index')
+          @content_blocks[index] = data.fetch('content_block')
+        when 'content_block_delta'
+          parse_content_delta(data)
+        when 'message_delta'
+          parse_message_delta(data)
+        when 'message_stop'
+          @complete = true
+        end
+      end
+      # rubocop:enable Metrics/MethodLength
+
+      # Convert to format expected by InvokeModelResponse
+      def to_response_format
+        message.merge({ 'content' => content_blocks })
+      end
+
+      private
+
+      def parse_content_delta(data)
+        index = data.fetch('index')
+        if data.dig('delta', 'type') == 'text_delta'
+          @content_blocks[index]['text'] += data.dig('delta', 'text')
+        end
+      end
+
+      def parse_message_delta(data)
+        @message.merge!(data.fetch('delta'))
+        @message['usage'] = @message['usage'].merge(data.fetch('usage', {}))
+      end
+    end
+  end
+end
