995458ef7a14e02cbb1e04be0509cf83dc5d987b
Alex Miller <apmiller108@yahoo.com>
2025-03-07 22:02:52 -0600

    Adds stream option

--- lib/generative_text/anthropic/invoke_model_request.rb
+++ lib/generative_text/anthropic/invoke_model_request.rb
@@ -1,18 +1,20 @@
 class GenerativeText
   module Anthropic
     class InvokeModelRequest
-      attr_reader :generate_text_request
+      attr_reader :generate_text_request, :stream
 
       delegate :model, :prompt, :temperature, :file, :conversation, :system_message, to: :generate_text_request
 
-      def initialize(generate_text_request)
+      def initialize(generate_text_request, stream: false)
         @generate_text_request = generate_text_request
+        @stream = stream
       end
 
       def to_h
         {
           model: model.api_name,
           max_tokens: model.max_tokens,
+          stream:,
           system: system_message,
           tools:,
           tool_choice: { type: :auto },
