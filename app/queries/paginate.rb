dbc545fb36aab37b47d63034fc738920a06b9b1b
Alex Miller <apmiller@covermymeds.com>
2025-04-04 13:59:22 -0500

    Renames method, adds docs and changes count strategy

--- app/queries/paginate.rb
+++ app/queries/paginate.rb
@@ -2,8 +2,16 @@
 # PaginationComponent to produce infinite scrolls lists.
 module Paginate
   class << self
-    # Cursor based pagination
-    def call(relation:, limit:, cursor:, order: { created_at: :desc })
+    # Implements cursor-based pagination for ActiveRecord relations.
+    #
+    # @param relation [ActiveRecord::Relation] The base relation to paginate
+    # @param limit [Integer] Maximum number of records to return
+    # @param cursor [Integer, nil] ID of the last record from the previous page, or nil for the first page
+    # @param order [Hash] Sorting order, defaults to created_at: :desc
+    #
+    # @return [Array<Array, Integer, nil>] Array containing records and the next cursor value (nil if no more records)
+    # @note The cursor record itself is not included in the returned records
+    def cursor(relation:, limit:, cursor:, order: { created_at: :desc })
       # Increment by 1 to get the next cursor. The "cursor" record is not returned
       # as part of the records set.
       relation = relation.order(order).limit(limit + 1)
@@ -17,15 +25,30 @@ module Paginate
       [records, cursor]
     end
 
-    # Page and offset pagination
+    # Implements traditional page/offset-based pagination for ActiveRecord relations.
+    #
+    # @param relation [ActiveRecord::Relation] The base relation to paginate
+    # @param page [Integer] Page number to retrieve (starts at 1)
+    # @param per_page [Integer] Number of records per page, minimum 1, defaults
+    # to 15. 0 if we just need the metadata for initialization (useful for the
+    # PaginationComponent)
+    # @param order [Hash] Sorting order, defaults to created_at: :desc
+    #
+    # @return [Array<Array, Hash>] Array containing records and pagination metadata
+    # @option return[1] :current_page [Integer] Current page number
+    # @option return[1] :per_page [Integer] Records per page
+    # @option return[1] :total_count [Integer] Total number of records
+    # @option return[1] :total_pages [Integer] Total number of pages
+    # @option return[1] :has_next_page [Boolean] Whether there is a next page
+    # @option return[1] :has_prev_page [Boolean] Whether there is a previous page
     def paginate(relation:, page: 0, per_page: 15, order: { created_at: :desc })
       page = page.to_i
       per_page = [per_page.to_i, 1].max
       offset = [(page - 1), 0].max * per_page
-      # plucking ID to reset any select query customizations (eg,
+      # Reslect ID to reset any select query customizations (eg,
       # neighbor_distance) which makes generates invalid SQL when combined with
       # `count`
-      total_count = relation.pluck(:id).count
+      total_count = relation.dup.reselect(:id).count
       total_pages = (total_count.to_f / per_page).ceil
 
       records = relation.order(order).limit(per_page).offset(offset)
