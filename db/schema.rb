f3f3bf85b74f94a5c3d6613f21a3113fb6d0bfe7
Alex Miller <apmiller108@yahoo.com>
2025-04-30 20:56:33 -0500

    Updates schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -225,6 +225,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_25_155920) do
     t.string "provider"
     t.string "uid"
     t.boolean "developer", default: false, null: false
+    t.vector "embedding", limit: 1024
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
