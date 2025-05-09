5885f1106e1cb5938b671b85ec6acbfdf2e85417
Alex Miller <apmiller@covermymeds.com>
2025-05-09 14:37:58 -0500

    Adds tool switch

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
   def list_dom_id(record)
     dom_id(record, 'list')
   end
+
+  def tool_type_description(tool_type)
+    case tool_type.to_sym
+    when :image
+      'Enable AI image generation capabilities for this conversation'
+    else
+      "Enable the #{tool_type.titleize} tools for this conversation"
+    end
+  end
 end
