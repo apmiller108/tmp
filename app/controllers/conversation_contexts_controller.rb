37f82cfa063d41cdfcc144640e2c8f616d16dfe2
Alex Miller <apmiller108@yahoo.com>
2025-07-11 22:51:22 -0500

    Adds destroy method

--- app/controllers/conversation_contexts_controller.rb
+++ app/controllers/conversation_contexts_controller.rb
@@ -15,9 +15,24 @@ class ConversationContextsController < ApplicationController
   end
 
   def index
-    @contexts = @conversation.conversation_contexts.order(created_at: :desc)
+    @contexts = @conversation.contexts.order(created_at: :desc)
     respond_to do |format|
       format.json { render json: @contexts, status: :ok }
+    end
+  end
+
+  def destroy
+    @context = @conversation.contexts.find(params[:id])
+
+    respond_to do |format|
+      if @context.destroy
+        format.json { head :no_content }
+      else
+        fomat.json do
+          render json: { error: 'Unable to delete context' },
+                 status: :unprocessable_entity
+        end
+      end
     end
   end
 
