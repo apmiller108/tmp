5b9a400a3874e8f7e26864358a139c657c625fc4
Alex Miller <apmiller108@yahoo.com>
2025-07-20 18:08:34 -0500

    Adds pagination to contexts

--- app/controllers/conversation_contexts_controller.rb
+++ app/controllers/conversation_contexts_controller.rb
@@ -3,16 +3,25 @@ class ConversationContextsController < ApplicationController
   before_action :set_conversation_context, only: [:destroy]
 
   def index
-    @conversation_contexts = current_user.conversation_contexts.includes(:conversations)
+    @search_params = search_params
+    relation = current_user.conversation_contexts.includes(:conversations)
 
-    if params[:search].present?
-      @conversation_contexts = @conversation_contexts.where(
+    if @search_params.dig(:q, :search).present?
+      relation = relation.where(
         'filename ILIKE ? OR mime_type ILIKE ?',
-        "%#{params[:search]}%", "%#{params[:search]}%"
+        "%#{search_params[:q][:search]}%", "%#{search_params[:q][:search]}%"
       )
     end
 
-    @conversation_contexts = @conversation_contexts.order(created_at: :desc)
+    @conversation_contexts, @pagination = Paginate.paginate(relation:,
+                                                            page: params.fetch(:page, 1),
+                                                            per_page: 10,
+                                                            order: { created_at: :desc })
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
   end
 
   def destroy
@@ -35,6 +44,10 @@ class ConversationContextsController < ApplicationController
   end
 
   private
+
+  def search_params
+    params.permit(:format, :page, :per_page, q: %i[search])
+  end
 
   def set_conversation_context
     @conversation_context = current_user.conversation_contexts.find(params[:id])
