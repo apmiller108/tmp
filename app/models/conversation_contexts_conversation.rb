718b9c2de7b28864dec7a71b12b6f58783f2fcd5
Alex Miller <apmiller108@yahoo.com>
2025-07-20 21:23:09 -0500

    Fixes inverse declarations

--- app/models/conversation_contexts_conversation.rb
+++ app/models/conversation_contexts_conversation.rb
@@ -1,5 +1,5 @@
 class ConversationContextsConversation < ApplicationRecord
   belongs_to :conversation
   belongs_to :context, class_name: 'ConversationContext', foreign_key: 'conversation_context_id',
-                       inverse_of: :conversation_contexts_conversation
+                       inverse_of: :conversation_contexts_conversations
 end
